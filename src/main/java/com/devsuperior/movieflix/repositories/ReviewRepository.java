cbfa48886fa56ee85a8f86cffcbd9defb6bb5082
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-04-09 12:26:23 -0300

    ADD Repositorie

---
+++ src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Long, ReviewRepository> {
+    
+
+}
